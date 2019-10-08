ca34ee84f531aaf4a2df7a1a5077bb1ca4d80ee1
gouzal <larbi-inco@live.fr>
2019-10-08 23:38:04 +0100

    integrate login page

---
+++ src/app/login/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+declare var $: any;
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    document.body.className = 'hold-transition login-page';
+    // $(() => {
+    //   $('input').iCheck({
+    //     checkboxClass: 'icheckbox_square-blue',
+    //     radioClass: 'iradio_square-blue',
+    //     increaseArea: '20%' /* optional */
+    //   });
+    // });
+  }
+
+}
